fabf3bdafa60d9dcfb5dca6b5bd51fd2d4b7485f
laujamie <36782341+laujamie@users.noreply.github.com>
2020-03-19 21:40:00 +0000

    Add news reader

---
+++ src/containers/StoriesContainer.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState, memo } from "react";
+import { getStoryIds } from "../services/hnApi";
+import Story from "../components/Story";
+import {
+  GlobalStyle,
+  StoriesContainerWrapper
+} from "../styles/StoriesContainerStyles";
+import useInfiniteScroll from "../hooks/useInfiniteScroll";
+
+const StoriesContainer = () => {
+  const { count } = useInfiniteScroll();
+  const [storyIds, setStoryIds] = useState([]);
+  useEffect(() => {
+    getStoryIds().then(data => {
+      setStoryIds(data);
+    });
+  }, []);
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.slice(0, count).map(storyId => (
+          <Story key={storyId} storyId={storyId}></Story>
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
+};
+
+export default StoriesContainer;
---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+export const singularStory = {
+  by: "Jamie Lau",
+  id: 1,
+  time: 1567209822,
+  title: "React Sorting Visualizer",
+  url: "https://jamielau.me/projects/sorting-visualizer"
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined
+};
