c9909ce4ad917ed5c963e6f8b03e3da9e2a6a477
laujamie <36782341+laujamie@users.noreply.github.com>
2020-03-19 22:00:51 +0000

    Updates

---
+++ precache-manifest.ff5cc5a1cae3758fa61d1b29aeb959ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7353fd6589d1196f24d307405ec6c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21caa2451733ddcad103",
+    "url": "/static/js/2.62df88eb.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.62df88eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5ab57546a9a810e8c4",
+    "url": "/static/js/main.7635b4da.chunk.js"
+  },
+  {
+    "revision": "cbc28dcc8a813d9587c9",
+    "url": "/static/js/runtime-main.8feab291.js"
+  }
+]);
